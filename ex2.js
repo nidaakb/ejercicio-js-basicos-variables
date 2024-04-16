e10d4fde2d9667a8a6d0d533a0629a127214731d
nidaakb <nidaakabbaj1997@gmail.com>
2024-04-16 21:20:29 +0200

    Ejercicio 1 y 2 solucion

--- ex2.js
+++ ex2.js
@@ -1,5 +1,6 @@
 /**
  * Que sucede aquí?
+ * Se estan proporcionando unos valores inciales a las variables y luego se asigna un nuevo valor a esas variables.
  */
 
  let variable1 = 1;
@@ -11,13 +12,21 @@
  variable4= 1;
  variable1= -7;
 
+ console.log(variable4)
+
  /**
   * En este punto, que valor tendrá 'variable4'. ¿Por qué? ¿Cómo podrías comprobarlo?
+  * El valor de la varibale4 seria -14 ya que:
+  * variable2 se convierte en -7
+  * variable4 se convierte en -7 * 2
   */
 
   variable2 = variable1;
   variable4 = variable2 * 2;
 
+
 /**
   * En este punto, que valor tendrá 'variable4'. ¿Por qué? ¿Cómo podrías comprobarlo?
   */
+
+console.log(variable4)
